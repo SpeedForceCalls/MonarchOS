f34437bf1d0459ab1266d9a0b5e301a062bfc130
SpeedForceCalls <darknessdarkness107@gmail.com>
2025-09-25 22:09:14 +0300

    Update base.cpp
    updated some frame issues
    
    + supports dx11

--- base.cpp
+++ base.cpp
@@ -18,7 +18,7 @@ HRESULT __stdcall hkPresent(IDXGISwapChain *pSwapChain, UINT SyncInterval, UINT 
 {
     if (!pDevice)
     {
-        // İlk çağrıda cihaz ve context al
+       
         DXGI_SWAP_CHAIN_DESC sd;
         pSwapChain->GetDesc(&sd);
         pSwapChain->GetDevice(__uuidof(ID3D11Device), (void **)&pDevice);
@@ -103,4 +103,5 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason, LPVOID)
     }
     return TRUE;
 }
+
 
